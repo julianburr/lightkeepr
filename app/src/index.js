a3ef08b8e960da0ae0d013318a02200733799875
Julian Burr <julian.burr@rexsoftware.com.au>
2021-02-03 17:11:42 +1000

    wip

--- app/src/index.js
+++ app/src/index.js
@@ -1,22 +1,30 @@
 import "./utils/firebase";
 
 import { StrictMode, Suspense } from "react";
-import { render } from "react-dom";
+import { render, hydrate } from "react-dom";
 import { FirebaseProvider } from "react-firebase-context";
 
 import { App } from "./app";
 import { reportWebVitals } from "./utils/report-web-vitals";
 
-render(
-  <StrictMode>
-    <Suspense fallback={<p>Loading...</p>}>
-      <FirebaseProvider>
-        <App />
-      </FirebaseProvider>
-    </Suspense>
-  </StrictMode>,
-  document.getElementById("root")
-);
+function Root() {
+  return (
+    <StrictMode>
+      <Suspense fallback={<p>Loading...</p>}>
+        <FirebaseProvider>
+          <App />
+        </FirebaseProvider>
+      </Suspense>
+    </StrictMode>
+  );
+}
+
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(<Root />, rootElement);
+} else {
+  render(<Root />, rootElement);
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
