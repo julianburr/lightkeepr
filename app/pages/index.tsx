9cb7cffd4b92c652e801cb20534005ceac43ad23
Julian Burr <hello@julianburr.de>
2022-01-19 17:40:01 +1000

    fix

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,7 +1,31 @@
-function AuthLayout() {
-  return <p>TODO</p>;
+import { PropsWithChildren } from "react";
+import { useAuthUser } from "src/hooks/use-auth-user";
+
+type AuthLayoutProps = PropsWithChildren<Record<never, any>>;
+
+function AuthLayout({ children }: AuthLayoutProps) {
+  const authUser = useAuthUser();
+  console.log({ authUser });
+
+  if (!authUser.uid) {
+    return <p>Login screen</p>;
+  }
+
+  if (!authUser.user) {
+    return <p>User setup</p>;
+  }
+
+  if (!authUser.organisationUser) {
+    return <p>Org setup</p>;
+  }
+
+  return <>{children}</>;
 }
 
 export default function HomeScreen() {
-  return <AuthLayout />;
+  return (
+    <AuthLayout>
+      <p>Redirect to org dashboard</p>
+    </AuthLayout>
+  );
 }
