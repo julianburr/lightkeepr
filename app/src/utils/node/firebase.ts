17854c8fd2b0e6d091f143f32a1d7fc266eb3a9b
Julian Burr <hello@julianburr.de>
2022-02-05 01:20:58 +1000

    fix: fix cleanup cronjob

--- app/src/utils/node/firebase.ts
+++ app/src/utils/node/firebase.ts
@@ -1,12 +1,10 @@
-import { cert, initializeApp, getApp } from "firebase-admin/app";
+import { cert, initializeApp } from "firebase-admin/app";
 import credentials from "google-service-account.json";
 
-if (!getApp()?.name) {
-  initializeApp({
-    credential: cert({
-      projectId: credentials.project_id,
-      clientEmail: credentials.client_email,
-      privateKey: credentials.private_key,
-    }),
-  });
-}
+initializeApp({
+  credential: cert({
+    projectId: credentials.project_id,
+    clientEmail: credentials.client_email,
+    privateKey: credentials.private_key,
+  }),
+});
